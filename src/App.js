568492330f1ffe7bc302fe3acdc7b2c949a1bb1b
cpon00 <cpon712@gmail.com>
2020-02-27 19:06:40 -0800

    added map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
-import Header from "./Header"
+import Header from "./Header";
+import Map from "./Map";
 
 export default function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      <Map />
     </div>
   );
 }
-
