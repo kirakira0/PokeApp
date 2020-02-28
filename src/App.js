f5421414a0f45e8850e2ceaf8a6ad4d6fcbd1fa7
kirakira0 <kiratoal0@gmail.com>
2020-02-27 19:02:52 -0800

    Created components for Results, Map, Questions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { useEffect, useState } from "react";
 import Header from "./Header"
+import Results from "./Results"
+import Map from "./Map"
+import Questions from "./Questions"
+
+
 
 export default function App() {
   return (
     <div className="App">
       <Header/>
+      <Results/> 
+      <Map/>
+      <Questions/>
     </div>
   );
 }
