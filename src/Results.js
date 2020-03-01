5bc5cadc4b8a719553f1b6e53bbaeab4d32b15a0
kirakira0 <kiratoal0@gmail.com>
2020-03-01 12:38:30 -0800

    Formatted results component

--- src/Results.js
+++ src/Results.js
@@ -1,7 +1,15 @@
 import React from "react"
 
+import Container from '@material-ui/core/Container'
+import Typography from '@material-ui/core/Typography'
+import Paper from '@material-ui/core/Paper'
+
 export default function Results() {
     return (
-    <h1>The Pokemon best suited to fight your enemy is: {'GetPoke.name'} </h1>
-        )
+    <Container maxWidth="sm" style={{marginTop: 20}}>
+      <Paper style={{padding: 20, backgroundColor: '#fffff3'}}>
+        <Typography variant="h3" align="center">The Pokemon best suited to fight your enemy is: {'GetPoke.name'} </Typography>
+      </Paper>
+    </Container>
+    )
 }
