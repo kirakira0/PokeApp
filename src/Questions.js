3045d312404edba61d019c4df57b252e4cd499c5
kirakira0 <kiratoal0@gmail.com>
2020-03-01 21:21:16 -0800

    Created lists for questions, answers, and images

--- src/Questions.js
+++ src/Questions.js
@@ -6,19 +6,29 @@ import Paper from '@material-ui/core/Paper'
 
 export default function Results() {
 
+    //structure of the questions and answers {question: [api-value, [answer1, value], [answer2, value]]}
+    var questions = [
+        "Which character trait best describes your opponent?"
+    ]
+    var answers = [
+        ["type", ["Stubborn and Hot-Headed", "water"], ["Calm and Quiet", "electric"], ["Arrogant and Selfish", "psychic"], ["Friendly and Attention-Seeking", "ground"], ],
+    ]
+
+    var images = [
+        "https://i.ya-webdesign.com/images/pokemon-clip-small-16.png"
+    ]
+    
     return (
         <Container maxWidth="lg" style={{marginTop: 20}}>
         <Paper style={{padding: 10, backgroundColor: '#fffff3'}}>
-            <img src="https://i.ya-webdesign.com/images/pokemon-clip-small-16.png" alt="new"/>
+            <img src= {images[0]} alt="new"/>
             <Typography variant="h3" align="center">
-                What is your enemy’s defining quality? 
+                {questions[0]}
             </Typography>
-            <div>
-          <button>Hot-tempered</button>
-        </div>
-        <div>
-          <button>Relaxed</button>
-        </div>
+            <button>{answers[0][1][0]}</button>
+            <button>{answers[0][2][0]}</button>
+            <button>{answers[0][3][0]}</button>
+            <button>{answers[0][4][0]}</button>
         </Paper>
       </Container>
     
