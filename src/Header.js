3c50b8892b74f3cf7fec47adb1e6d8ed8bb1062f
stesfu <salemtesfu@gmail.com>
2020-03-05 02:36:02 -0800

    Added page description and fixed spacing

--- src/Header.js
+++ src/Header.js
@@ -5,6 +5,7 @@ export default function Header() {
     return (
         <div id="header-div">
             <h1>POKETEST!</h1>
+            <p>Answer some questions about your greatest enemy and we'll return a pokemon best suited to fight them!</p>
         </div>
     )
 }
